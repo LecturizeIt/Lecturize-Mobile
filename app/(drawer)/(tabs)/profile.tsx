c8780374e81ee225128194a546601594262f5507
Miralhas <victor2.miralhas@gmail.com>
2025-04-13 22:25:59 +0000

    feat: prebuild expo app

--- app/(drawer)/(tabs)/profile.tsx
+++ app/(drawer)/(tabs)/profile.tsx
@@ -1,17 +1,11 @@
 import ProfileList from '@/components/profile-list';
 import ProtectedRoute from '@/components/protected-route';
-import SuspenseLoading from '@/components/suspense-loading';
 import { useAuthContext } from '@/contexts/auth-context';
 import { View } from 'react-native';
 
 
 const ProfilePage = () => {
-  const { user, isLoading } = useAuthContext();
-
-  if (isLoading) {
-    return <SuspenseLoading />
-  }
-
+  const { user } = useAuthContext();
   return (
     <ProtectedRoute directTo="/login">
       <View className="flex flex-1 px-10 w-full pt-4 gap-6">
